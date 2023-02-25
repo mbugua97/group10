d5921000a79996b72c59f5e07174b3e811feccd6
Stephen Mwaniki <mwanikis039@gmail.com>
2023-02-26 02:38:35 +0300

    Added Reverse String Task

---
+++ software/ReverseString/ReverseString.py
@@ -0,0 +1,8 @@
+# Get user input for the String to be Reversed
+string_to_reverse=input("ENTER A STRING YOU WANT TO REVERSE:")
+
+# Reverse the String using slicing
+reversed_string=string_to_reverse[::-1]
+
+# Print the reversed string
+print("Reversed String:",reversed_string)
